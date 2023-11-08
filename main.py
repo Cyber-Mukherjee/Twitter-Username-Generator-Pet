3e494f74b218bc82097a2bdac1a150087d44a500
Gaurav-Mukherjee <gauravmukherjee69@gmail.com>
2023-11-08 18:42:21 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Twitter username generator
+
+print("Namastey People\nWelcome to Gaurav's Twitter Username Generator Project\n")
+
+pet_name = input("Please enter your pet's name: ").lower()
+birth_city = input("Please enter the name of the city you were born in: ").title()
+
+twitter_handle = "cyber"+pet_name
+
+print(f"\nYour new twitter handle and bio @{twitter_handle} from {birth_city}")
